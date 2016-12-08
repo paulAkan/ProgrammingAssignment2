0730ca5da1ad749e1a9f732f94f7fb8fcb062ec5
paulAkan <akangahpaul@yahoo.com>
2016-12-08 09:58:12 -0500

    week 03 matrix inversion program

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this function stores and set the matrix and its inverse
+## it is an input to another function cacheSolve
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setm <- function(inverse) m <<- inverse
+        getm <- function() m
+        list(set=set, get = get, setm = setm, getm = getm)
 }
 
 
-## Write a short comment describing this function
+## this function checks if the inverse exits
+##if the inverse exits, it is recall from cache
+##otherwise, it is calculated
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getm()
+        if (!is.null(m)){
+                message("getting cached data...")
+                return(m)
+        }
+        ##find the inverse of the matrix
+        data <-x$get()
+        m <- solve(data, ...)
+        x$setm(m)
+        return(m)
 }
